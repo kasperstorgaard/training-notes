b7e9eb3709cf54235adfd44ad411358fedba06c4
kstorgaard <kst@kayak.com>
2018-11-14 22:21:44 +0100

    playing around with styles/layout

--- client/components/nav/nav-item.js
+++ client/components/nav/nav-item.js
@@ -1,22 +1,47 @@
 import { LitElement, html } from '@polymer/lit-element';
-import { classMap } from 'lit-html/directives/classMap';
+
+import style from './nav-item.css';
+import { onNavigate } from '../../router';
 
 export class NavItem extends LitElement {
   static get properties() {
     return {
-      url: String
+      url: String,
+      active: {
+        type: Boolean,
+        reflect: true
+      }
     };
   }
-  
-  render() {
-    const classes = { active: false };
 
+  connectedCallback() {
+    super.connectedCallback();
+    this._unsub = onNavigate(() => this.active = this._isActive());
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    this._unsub();
+  }
+
+  render() {
     return html`
-      <a href="${this.url}" class="${classMap(classes)}">
-        <slot></slot
-      </a>
+      <style>${style}</style>
+      <a href="${this.url}"><slot></slot</a>
     `;
   }
+
+  _isActive() {
+    const pathname = window.location.pathname;
+
+    if (!pathname.startsWith(this.url)) {
+      return false;
+    }
+
+    return pathname.length === this.url.length ||
+      pathname[this.url.length] === '/';
+  }
+
 }
 
 customElements.define('tn-nav-item', NavItem);
--- client/main.js
+++ client/main.js
@@ -1,5 +1,5 @@
 import styleInject from 'style-inject';
 import './router';
-import main from './main.scss';
+import main from './main.css';
 
 styleInject(main);
