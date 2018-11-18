e57733910b7e998ab6d217dee5e3cecc2faa0a60
kstorgaard <kst@kayak.com>
2018-11-18 09:50:46 +0100

    add components page for development

--- routes.js
+++ routes.js
@@ -8,4 +8,8 @@ router.get('/', routerHelper('pages/home/home', {}));
 router.get('/calendar', routerHelper('pages/calendar/calendar', {}));
 router.get('/exercises', routerHelper('pages/exercises/exercises', {}));
 
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/components', routerHelper('pages/components/components', {}));
+}
+
 module.exports = router;
---
+++ views/pages/components/components.js
@@ -0,0 +1,5 @@
+import styleInject from 'style-inject';
+
+import styles from './components.css';
+
+styleInject(styles);
