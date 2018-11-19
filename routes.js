598fb1f09ac16d76280c92ebff902b7c528fad5d
kstorgaard <kst@kayak.com>
2018-11-19 21:18:23 +0100

    add page name class for index

--- routes.js
+++ routes.js
@@ -4,12 +4,12 @@ const {routerHelper} = require('./services/router-helper');
 
 const router = express.Router();
 
-router.get('/', routerHelper('pages/home/home', {}));
-router.get('/calendar', routerHelper('pages/calendar/calendar', {}));
-router.get('/exercises', routerHelper('pages/exercises/exercises', {}));
+router.get('/', routerHelper('pages/home/home', {name: 'home'}));
+router.get('/calendar', routerHelper('pages/calendar/calendar', {name: 'calendar'}));
+router.get('/exercises', routerHelper('pages/exercises/exercises', {name: 'exercises'}));
 
 if (process.env.NODE_ENV !== 'production') {
-  router.get('/components', routerHelper('pages/components/components', {}));
+  router.get('/components', routerHelper('pages/components/components', {name: 'components'}));
 }
 
 module.exports = router;
