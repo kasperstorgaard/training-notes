f3098da406bdaadb6ba5ef150e86e04c16e7165b
kstorgaard <kst@kayak.com>
2018-11-20 16:54:51 +0100

    merge resources/styles build scripts

--- resources.js
+++ resources.js
@@ -5,6 +5,7 @@ const mkdirp = require('mkdirp');
 const {promisify} = require('util');
 const {argv} = require('yargs')
 const chokidar = require('chokidar');
+const uglifycss = require('uglifycss');
 
 const copyFileAsync = promisify(fs.copyFile);
 const unlinkAsync = promisify(fs.unlink);
@@ -14,12 +15,13 @@ const mkdirpAsync = promisify(mkdirp)
 
 const baseDir = path.join(__dirname, 'public');
 const isWatch = argv.w || argv.watch;
+const isProd = process.env.NODE_ENV === 'production';
 
-const pattern = './app/**/*.@(woff|woff2|png|jpg|jpeg)';
+const pattern = './app/**/*.@(woff|woff2|png|jpg|jpeg|css)';
 
 (async function run() {
   const filePaths = await globAsync(pattern);
-  filePaths.forEach(filePath => copyFile(filePath));
+  filePaths.forEach(filePath => processFile(filePath));
 
   if (isWatch) {
     const watcher = chokidar.watch(pattern, {ignoreInitial: true});
@@ -27,11 +29,11 @@ const pattern = './app/**/*.@(woff|woff2|png|jpg|jpeg)';
     watcher
       .on('add', filePath => {
         console.log(`resource added: ${filePath}`);
-        copyFile(filePath);
+        processFile(filePath);
       })
       .on('change', filePath => {
         console.log(`resource changed: ${filePath}`);
-        copyFile(filePath);
+        processFile(filePath);
       })
       .on('unlink', filePath => {
         console.log(`resource deleted: ${filePath}`);
@@ -45,17 +47,27 @@ const pattern = './app/**/*.@(woff|woff2|png|jpg|jpeg)';
   }
 }());
 
-async function copyFile(src) {
-  const dest = getDest(src);
-  if (!(await existsAsync(dest))) {
-    await mkdirpAsync(path.dirname(dest));
+function processFile(src) {
+  const type = getType(src);
+  switch (type) {
+    case 'woff':
+    case 'woff2':
+      return copyFile(src);
+    case 'png':
+    case 'jpg':
+    case 'jpeg':
+    case 'svg':
+      return copyFile(src);
+    case 'css':
+      return isProd ? uglifyStyles(src) : copyFile(src);
+    default:
+      return copyFile(src);
   }
-  return copyFileAsync(src, getDest(src));
 }
 
-function getDest(relativeSrc) {
-  const type = relativeSrc.match(/\.([^\.]+)$/)[1];
-  const basePath = path.join(baseDir, relativeSrc);
+function getDest(src) {
+  const type = getType(src);
+  const basePath = path.join(baseDir, src);
   return basePath.replace(/\/app\//, () => {
     switch (type) {
       case 'woff':
@@ -66,8 +78,32 @@ function getDest(relativeSrc) {
       case 'jpeg':
       case 'svg':
         return '/images/';
+      case 'css':
+        return '/css/'
       default:
         return '/resources/';
     }
   })
+}
+
+function getType(src) {
+  return src.match(/\.([^\.]+)$/)[1];
+}
+
+async function copyFile(src) {
+  const dest = getDest(src);
+  if (!(await existsAsync(dest))) {
+    await mkdirpAsync(path.dirname(dest));
+  }
+  return copyFileAsync(src, getDest(src));
+}
+
+async function uglifyStyles(src) {
+  const dest = getDest(src);
+  const content = await readFileAsync(src, 'utf8');
+  const output = uglifycss.processString(content);
+  if (!(await existsAsync(dest))) {
+    await mkdirpAsync(path.dirname(dest));
+  }
+  return writeFileAsync(getDest(src), output, 'utf8');
 }
